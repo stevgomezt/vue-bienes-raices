75c3348eb7e669e569d77a71be67160204f6c656
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2024-05-07 09:06:34 -0500

    236

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,31 @@
 <script setup></script>
 
 <template>
-      <h1 class="text-center text-h3 font-weight-bold my-5">Iniciar Sesion</h1>
+    <v-card flat max-width="600" class="mx-auto my-10">
+        <v-card-title class="text-h4 font-weight-bold">
+            Iniciar Sesion
+        </v-card-title>
+
+        <v-card-subtitle class="text-h5 font-weight-bold">
+            Inicia Sesion con tu Cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+            <v-text-field
+                type="email"
+                label="Email"
+                bg-color="blue-grey-lighten-5"
+            />
+
+            <v-text-field
+                type="password"
+                label="Password"
+                bg-color="blue-grey-lighten-5"
+            />
+
+            <v-btn block color="pink-accent-3"> Iniciar Sesion </v-btn>
+        </v-form>
+    </v-card>
 </template>
 
-<style>
-</style>
+<style></style>
